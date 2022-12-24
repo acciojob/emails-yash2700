eb78315eaf18e17bd350dfec2c7935a8c0a32cc5
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2022-12-24 21:53:50 +0530

    1st commit

--- src/main/java/com/driver/Meeting.java
+++ src/main/java/com/driver/Meeting.java
@@ -6,6 +6,15 @@ public class Meeting {
     private LocalTime startTime;
     private LocalTime endTime;
 
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
     public Meeting(LocalTime startTime, LocalTime endTime){
         this.startTime = startTime;
         this.endTime = endTime;
